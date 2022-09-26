297c811df50d1e8ae996771f40c96ac1a3b935a3
Eduardo Andrade <100982195+Duhandrade22@users.noreply.github.com>
2022-09-26 17:34:18 -0300

    Busca de nomes dos pokemons em tela

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Searchbar from './components/Searchbar';
-
+import React, { useEffect, useState } from "react";
+import { getPokemons, getPokemonsData } from "./api";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Pokedex from "./components/Pokedex";
+import Searchbar from "./components/Searchbar";
 
 function App() {
 
+  const [loading, setLoading] = useState(false)
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true)
+      const data = await getPokemons()
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonsData(pokemon.url)
+      })
+
+      const results = await Promise.all(promises)
+      setPokemons(results)
+      setLoading(false)
+    } catch (error) {
+      console.log("fetchPokemons error: ", error)
+    }
+  }
+  useEffect(() => {
+    console.log("Carregou");
+    fetchPokemons();
+  }, []);
 
   return (
     <div>
       <Navbar />
-      <Searchbar/>
-      <div className="App">
-      </div>
+      <Searchbar />
+      <Pokedex pokemons={pokemons} loading={loading}/>
     </div>
   );
 }
