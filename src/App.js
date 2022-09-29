3c508bff9912780cc5ace9286db6065341c82bcf
Eduardo Andrade <100982195+Duhandrade22@users.noreply.github.com>
2022-09-29 18:07:53 -0300

    paginação

--- src/App.js
+++ src/App.js
@@ -6,35 +6,43 @@ import Pokedex from "./components/Pokedex";
 import Searchbar from "./components/Searchbar";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
 
-  const [loading, setLoading] = useState(false)
-  const [pokemons, setPokemons] = useState([])
-
+  const itensPerPage = 25
   const fetchPokemons = async () => {
     try {
-      setLoading(true)
-      const data = await getPokemons()
+      setLoading(true);
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
-        return await getPokemonsData(pokemon.url)
-      })
+        return await getPokemonsData(pokemon.url);
+      });
 
-      const results = await Promise.all(promises)
-      setPokemons(results)
-      setLoading(false)
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage))
     } catch (error) {
-      console.log("fetchPokemons error: ", error)
+      console.log("fetchPokemons error: ", error);
     }
-  }
-  useEffect(() => {
-    console.log("Carregou");
-    fetchPokemons();
-  }, []);
+  };
+  useEffect (() => {
+    fetchPokemons()
+  }, [page])
 
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading}/>
+      <Pokedex
+        pokemons={pokemons}
+        loading={loading}
+        page={page}
+        setpage={setPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 }
